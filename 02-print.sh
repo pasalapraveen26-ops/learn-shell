9084dedef1e0d29db8174aa92e8c473dd4aef897
pasala26 <pasalapraveen26@gmail.com>
2025-08-25 13:23:57 +0530

    First shell script

---
+++ 02-print.sh
@@ -0,0 +1,15 @@
+# syntax : echo -e "\e[COLmMESSAGE\e[0m"
+# -e - enable colors
+# \e[COLm - to enable certain color
+#  \e[0m - to disable color which is enabled
+# COL stands for color and possible colors are RED(31), GREEN(32) , YELLOW(33) , BLUE(34), MAGENTA(35), CYAN(36)
+
+
+echo -e "\e[31mHello in Red color\e[0m"
+echo -e "\e[32mHello in Green color\e[0m"
+echo -e "\e[33mHello in Yellow color\e[0m"
+echo -e "\e[34mHello in Blue color\e[0m"
+echo -e "\e[35mHello in Magenta color\e[0m"
+echo -e "\e[36mHello in Cyan color\e[0m"
+
+
